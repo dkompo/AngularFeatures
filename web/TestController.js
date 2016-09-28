afea83a0610e3148f67174c8b5b8ccdfd955bd41
dkompo <jimkobe87@gmail.com>
2016-09-28 11:16:59 +0300

    Deleted a comment.

--- web/TestController.js
+++ web/TestController.js
@@ -10,7 +10,6 @@ app.controller('InvoiceController', ['timeDisplay', '$scope', function InvoiceCo
 		$scope.invoice.timeNow = response.data.time;
 	});
 
-	//var mitsos = 2;
 	//this.qty = 1;
 	this.cost = 2;
 	// $scope.invoice.qty = 4;
